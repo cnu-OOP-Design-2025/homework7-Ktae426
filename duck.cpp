34b11472071054199e9b53426b8c5e0bc361d97c
kktae <dmb09128@naver.com>
2025-11-04 13:40:10 +0900

    과제 제출

--- duck.cpp
+++ duck.cpp
@@ -1,22 +1,54 @@
 #include "duck.h"
 
+using namespace std;
+
 /* TODO */
-void Duck::performFly(){}
+void Duck::performFly(){
+    flyBehavior->fly();
+}
+
 /* TODO */
-void Duck::performQuack(){}
+void Duck::performQuack(){
+    quackBehavior->quack();
+}
+
 /* TODO */
-void Duck::setFlyBehavior(FlyBehavior* fb){}
+void Duck::setFlyBehavior(FlyBehavior* fb){
+    flyBehavior = fb;
+}
+
 /* TODO */
-void Duck::setQuackBehavior(QuackBehavior* qb){}
+void Duck::setQuackBehavior(QuackBehavior* qb){
+    quackBehavior = qb;
+}
 
 
 /* TODO */
-MallardDuck::MallardDuck() {}
+MallardDuck::MallardDuck() {
+    flyBehavior = new FlyWithWings();
+    quackBehavior = new Quack();
+}
+
 /* TODO */
-RedheadDuck::RedheadDuck() {}
+RedheadDuck::RedheadDuck() {
+    flyBehavior = new FlyWithWings();
+    quackBehavior = new Quack();
+}
+
 /* TODO */
-RubberDuck::RubberDuck() {}
+RubberDuck::RubberDuck() {
+    flyBehavior = new FlyNoWay();
+    quackBehavior = new Squeak();
+}
+
 /* TODO */
-DecoyDuck::DecoyDuck() {}
+DecoyDuck::DecoyDuck() {
+    flyBehavior = new FlyNoWay();
+    quackBehavior = new MuteQuack();
+}
+
 /* TODO */
-ModelDuck::ModelDuck() {}
+ModelDuck::ModelDuck() {
+    flyBehavior = new FlyNoWay();
+    quackBehavior = new MuteQuack();
+}
